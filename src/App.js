5b45554852d89e0454194994c62919e1a8f82248
developeranikhossin <mdmanunbiswas321@gmail.com>
2022-04-05 16:53:18 +0600

    react click event

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const Products = [
+    {name: "photoshop", price:"$90.99"},
+    {name: "alelastor", price:"$80.99"},
+    {name: "adobe", price:"$70.99"}
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <header>
+          <Product Product= {Products[0]}></Product>
+          <Product Product= {Products[1]}></Product>
+          <Product Product= {Products[2]}></Product>
+          <Counter></Counter>
+          <Users></Users>
+        </header>
     </div>
   );
 }
+
+function Counter(){
+  const [count, setCount] = useState(9);
+  const handleIncrease = () => {
+    // const newCount = count + 1;
+    setCount(count + 1);
+  };
+  const Decrease = () => {
+    // const newCount = count - 1;
+    setCount(count - 1);
+  };
+  return(
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={Decrease}>Decrease</button>
+      <button onMouseMove={handleIncrease}>handleIncrease</button>
+    </div>
+  )
+}
+
+function Users(){
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, [])
+  
+  return(
+    <div>
+      <h3>Dynamic users : {users.length}</h3>
+        <ul>
+            {
+              users.map(user => <li>{user.name}</li>)
+            }
+        </ul>
+    </div>
+  )
+}
+
+
+function Product(props){
+  const productStyle={
+    border: "1px solid gray",
+    borderRadius:"5px",
+    backgroundColor: "lightgray",
+    height:"200px",
+    width:"200px",
+    float:"left",
+    textAlign: "center",
+    padding: "50px"
+  }
+  return(
+    <div style={productStyle}>
+        <h3>{props.Product.name}</h3>
+        <h1>{props.Product.price}</h1>
+        <button>Buy Now</button>
+    </div>
+  )
+}
+
+
 
 export default App;
